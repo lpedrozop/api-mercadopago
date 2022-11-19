da1ebc18ccec5a60f29ea2a54783876f0a654945
lucas pedrozo <lukspedrozo@gmail.com>
2022-11-19 11:56:24 -0500

    update controller

--- src/controller/mercaControllers.js
+++ src/controller/mercaControllers.js
@@ -7,40 +7,48 @@ export const crearorden = async (req, res) =>{
     mercadopago.configure({
         access_token: ACCESS_TOKEN,
     });
-    // Crea un objeto de preferencia
 
-    let {items}= req.body
-    console.log(items)
-    let preference = {
-        "items": [...items],
-        "payer": {
-            "name": req.body.name,
-            "email": req.body.correo,
-        },
-        "back_urls": {
-            "success": "http://localhost:3000/feedback",
-            "failure": "http://localhost:3000/feedback",
-            "pending": "http://localhost:3000/feedback"
-        },
-        "auto_return": "approved",
-        "taxes":[{
-            "type": "IVA",
-            "value": 0
-        }],
-        "notification_url":"https://6b3a-186-82-84-0.ngrok.io/notificacion",
-        "statement_descriptor": "DOCS API"
-    };
+    let {items} = req.body
 
-    mercadopago.preferences
-        .create(preference)
-        .then(function (response) {
-            res.json({
-                pago_mer: response.body.init_point
+    if(!items){
+        res.status(403)
+        res.send({error: 'No se están enviando los items para la creación de la preferencia'})
+    }
+
+    else {
+        console.log(items)
+        let preference = {
+            "items": [...items],
+            "payer": {
+                "name": req.body.name,
+                "email": req.body.correo,
+            },
+            "back_urls": {
+                "success": "http://localhost:3000/feedback",
+                "failure": "http://localhost:3000/feedback",
+                "pending": "http://localhost:3000/feedback"
+            },
+            "auto_return": "approved",
+            "taxes": [{
+                "type": "IVA",
+                "value": 0
+            }],
+            "notification_url": "https://6b3a-186-82-84-0.ngrok.io/notificacion",
+            "statement_descriptor": "DOCS API"
+        };
+        mercadopago.preferences
+            .create(preference)
+            .then(function (response) {
+                res.json({
+                    pago_mer: response.body.init_point
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+                res.status(403)
+                res.send({error: 'Revise los datos para la creación de la orden, datos necesarios {quantity y unit_price'})
             });
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+    }
 }
 
 export const feedback = function (req, res){
